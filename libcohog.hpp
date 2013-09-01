bdddf602a55327bbc2274876c114a3c212381209
belltailjp <belltailjp@gmail.com>
2013-09-02 02:31:55 +0900

    全部includeできるヘッダを用意した

---
+++ libcohog.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <libcohog/CoHOGDetector.hpp>
+#include <libcohog/CoHOGParams.hpp>
+#include <libcohog/Detector.hpp>
+#include <libcohog/HOGDetector.hpp>
+#include <libcohog/Trainer.hpp>
+#include <libcohog/Verifier.hpp>
+
